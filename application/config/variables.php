7fc9018428784f2c9b2383175824ca57eaf964c7
= <=>
2014-10-11 23:38:05 +0200

    Dodanie pliku konfiguracyjnego variables;

---
+++ application/config/variables.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+*
+* @config: variables
+* dodatkowy plik konfiguracyjny.
+*
+*/
+
+$config = array (
+	'request_limit_min'		=> 20, // maksymalna liczba requestow bez captchy na minute
+	'request_limit_hour'	=> 200, // maksymalna liczba requestow bez captchy na godzine
+);
