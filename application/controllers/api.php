9bc5b24232728ac07e7cfb6c569efc63f79c80b3
Pethus Gethus <pethus@pethus.(none)>
2014-10-11 10:24:42 +0200

    Dodanie kontrolera API;

---
+++ application/controllers/api.php
@@ -0,0 +1,56 @@
+<?php
+
+class Api extends CI_Controller {
+
+	/*
+	*
+	* @function: index
+	* Funkcja przetwarza wszelkie żądania do API.
+	* Sprawdza limity ilości zapytań do API w określonym czasie, jeżeli zostaną przekroczone
+	* zwracamy użytkownikowi captche do wprowadzenia.
+	* 
+	*/
+
+	public function index () {
+
+		$this->load->model (array ('logger', 'limitter', 'request', 'response'));
+		$this->logger->debug ('Nowe zadanie: ' . $this->request->getRaw ());
+
+		if ($captcha = $this->request->getField ('captcha')) {
+			$this->load->model ('captcha');
+			if ($this->captcha->validate ($captcha))
+				$this->limitter->reset ();
+		}
+
+		if ($this->limitter->checkLimit ()) {
+
+			if (! $this->limitter->update ()) {
+				$this->log->fatal ('Nie udalo sie zaktualizowac limitu wywolan');
+				$this->response->fail ('Internal Database Error.');
+			}
+
+			if (! $this->request->pre_process ()) {
+				$this->log->fatal ('Bledne zadanie: ' . $this->request->getReason ());
+				$this->response->fail ('Bad Request.');
+			}
+
+			if (! $this->request->process ()) {
+				$this->log->fatal ('Nie udalo sie przetworzyc zadania: ' $this->request->getReason ());
+				$this->response->fail ('Internal Error.');
+			}
+
+			$this->logger->info ('Zadanie OK Typ: ' . $this->request->getModel () . ' Funkcja: ' . $this->Request->getFunction ());
+			$this->response->success ($this->request->getResponse ());
+
+		} else {
+
+			if (! $captcha = $this->captcha->makeNew ()) {
+				$this->logger->fatal ('Nie udalo sie stworzyc captchy: ' . $this->captcha->getReason ());
+				$this->response->fail ('Internal Error');
+			}
+
+			$this->response->captcha ($this->request->getAll (), $captcha);
+		}
+	}
+
+}
