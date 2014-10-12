eb9f8f1a3f1faaa4dd28165e63a167c215e39368
= <=>
2014-10-12 12:41:43 +0200

    Wypelnienie klasy response;

--- application/models/response.php
+++ application/models/response.php
@@ -2,11 +2,68 @@
 
 	/*
 	*
-	* @model Response
+	* @model: Response
 	* Klasa do wysyłania odpowiedzi zwrotnych do klienta.
 	*
 	*/
 
 class Response extends CI_Model {
+
+	/*
+	*
+	* @function: success
+	* Drukuje komunikat typu success.
+	*
+	*/
+
+	public function success ($message) {
+
+		return $this->_printMessage ($message, 'success');
+	}
+
+	/*
+	*
+	* @function: warning
+	* Drukuje komunikat typu warning.
+	*
+	*/
+
+	public function warning ($message) {
+
+		return $this->_printMessage ($message, 'warning');
+	}
+
+	/*
+	*
+	* @function: fail
+	* Drukuje komunikat typu fail.
+	*
+	*/
+
+	public function fail ($message) {
+
+		return $this->_printMessage ($message, 'fail');
+	}
+
+	/*
+	*
+	* @function: _printMessage
+	* Ustawia naglowki i drukuje komunikat.
+	*
+	*/
+
+	private function _printMessage ($msg = FALSE, $type = FALSE) {
+
+		if (! $msg OR ! $type) {
+			$this->logger->syntax ("Brak parametru wejsciowego Message: {$msg} Type: {$type}");
+		}
+
+		$message = array ('status' => $type, 'message' => $msg);
+
+		$this->output
+			->set_content_type ('application/json')
+			->set_output (json_encode ($message));
+
+	}
 
 }
