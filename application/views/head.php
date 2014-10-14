aed84c84bea3454772f585666fa507ff793b994e
= <=>
2014-10-14 21:21:36 +0200

    Dodanie brudnego widoku menu;

---
+++ application/views/head.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="utf-8">
+    <title>Online-tools.it</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="<?php echo base_url ('public/css/bootstrap.min.css'); ?>" media="screen">
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
+			<script src="../bower_components/respond/dest/respond.min.js"></script>
+		<![endif]-->
+		<script>
+		</script>
+	</head>
+	<body>
+		<div class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<a href="<?php echo base_url(); ?>" class="navbar-brand">online-tools.it</a>
+				</div>
+
+				<div class="navbar-collapse collapse" id="navbar-main">
+					<ul class="nav navbar-nav navbar-right">
+
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><span class="glyphicon glyphicon-road"></span> Domains &amp; Network<span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="themes">
+								<li><a href="<?php echo base_url('domains/whois'); ?>">Whois</a></li>
+								<li><a href="../cerulean/">Dropped</a></li>
+								<li><a href="../cosmo/">Records (DIG)</a></li>
+								<li><a href="../cosmo/">BlackList Checker</a></li>
+								<li class="divider"></li>
+								<li><a href="/domain">IP Lookup</a></li>
+								<li><a href="../cerulean/">Ping</a></li>
+								<li><a href="../cosmo/">Traceroute</a></li>
+							</ul>
+						</li>
+
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><span class="glyphicon glyphicon-tower"></span> Web Pages &amp; Sites <span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="themes">
+								<li><a href="../cosmo/">HTTP Requester</a></li>
+								<li><a href="../cosmo/">Web Proxy</a></li>
+								<li><a href="../cosmo/">Link Checker</a></li>
+								<li><a href="../cosmo/">Sitemap Generator</a></li>
+								<li><a href="../cosmo/">Web Scores</a></li>
+							</ul>
+						</li>
+
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><span class="glyphicon glyphicon-refresh"></span> Converters &amp; Parsers <span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="themes">
+								<li><a href="/domain">IP Lookup</a></li>
+								<li><a href="../cerulean/">Ping</a></li>
+								<li><a href="../cosmo/">Traceroute</a></li>
+								<li><a href="../cosmo/">HTTP Requester</a></li>
+								<li><a href="../cosmo/">Web Proxy</a></li>
+							</ul>
+						</li>
+
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><span class="glyphicon glyphicon-lock"></span> Security &amp; Encryption <span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="themes">
+								<li><a href="/domain">IP Lookup</a></li>
+								<li><a href="../cerulean/">Ping</a></li>
+								<li><a href="../cosmo/">Traceroute</a></li>
+								<li><a href="../cosmo/">HTTP Requester</a></li>
+								<li><a href="../cosmo/">Web Proxy</a></li>
+							</ul>
+						</li>
+
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><span class="glyphicon glyphicon-random"></span> Other &amp; Misc <span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="themes">
+								<li><a href="/domain">IP Lookup</a></li>
+								<li><a href="../cerulean/">Ping</a></li>
+								<li><a href="../cosmo/">Traceroute</a></li>
+								<li><a href="../cosmo/">HTTP Requester</a></li>
+								<li><a href="../cosmo/">Web Proxy</a></li>
+							</ul>
+						</li>
+
+					</ul>
+
+				</div>
+			</div>
+		</div>
+
+<div class"content">
+
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+<br /><br />
+
+</div>
+
+
+
+		<script src="<?php echo base_url ('public/js/jquery-2.1.1.min.js'); ?>"></script>
+		<script src="<?php echo base_url ('public/js/bootstrap.min.js'); ?>"></script>
+	
