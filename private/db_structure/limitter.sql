eef16a360c5b99cbdbcb9e25730c0ea0888a78df
Piotr Gebala <rajgu85@gmail.com>
2017-11-28 14:35:35 +0100

    Poprawka na limitter - struktura db

--- private/db_structure/limitter.sql
+++ private/db_structure/limitter.sql
@@ -1,6 +1,5 @@
-
 CREATE TABLE `limitter` (
   `ip` bigint(20) NOT NULL,
   `timestamp` int(11) NOT NULL,
   KEY `ip` (`ip`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
